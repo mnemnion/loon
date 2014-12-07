d3042f23984f64387da763ce484fc74f3a83033d
sam atman <atmanistan@gmail.com>
2014-12-07 12:13:16 -0800

    let us let

--- clua/idioms.lua
+++ clua/idioms.lua
@@ -2,25 +2,32 @@
 
 -- This takes strings, and interns them as values in the global environment.
 -- Both strings are interpreted as symbols
-function globalSetSyms (lval, rval)
+function _GSetSyms (lval, rval)
 	_G[lval] = _G[rval]
 end
---[[
-> foo = 23
-> bar = "fortytwo"
-> globalSet("foo","bar")
-> print(foo)
-fortytwo
---]] 
+--[[ > foo = 23
+	 > bar = "fortytwo"
+	 > globalSet("foo","bar")
+	 > print(foo)
+	 fortytwo --]] 
 
 -- This takes a string and a value, interning the value
-function globalSetVal(lval, rval)
+function _GSetVal(lval, rval)
 	_G[lval] = rval
 end
---[[
-> foo = 23
-> bar = "fortytwo"
-> globalSetVal("foo","bar")
-> print(foo)
-bar
---]] 
+--[[ > foo = 23
+	 > bar = "fortytwo"
+	 > globalSetVal("foo","bar")
+	 > print(foo)	
+	 bar --]] 
+
+-- This creates an anonymous function and returns it.
+function makeFn (fnArg, fnBody)
+	f = loadstring("return function("..fnArg..") "..fnBody.." end")
+	return f()
+end
+
+--[[ > f = makeFn("bar, baz", "return bar..baz")
+	 > _GSetVal("foo",f)
+	 > print(foo("hello"," function"))
+	 hello function --]]
---
+++ clua/notes/let.md
@@ -0,0 +1,26 @@
+#A Destructuring Let
+
+```clojure
+
+(let foo bar (...)) ; atomic let
+
+(let {foo bar,
+	  baz bux} (...)) ; compound let
+
+(let a (foo bar) (...)) ; atomic compound let
+
+(let {a (foo bar),
+	  b (baz bux)}) ; compound compound let
+
+(let [a,b,c] (foo bar) (...)) ; multiple return values
+
+(let { a (foo bar),
+	   [b,c,d] (baz bux)}) ; and so on
+
+(let (foo bar) (baz bux)) ; syntax error
+(let '(foo bar) (baz bux)) ; syntax error, macro resolves form from AST, 
+						   ; where '(list elements) and [vector elements]
+						   ; are not equivalent. 
+
+```
+
