c2eb77f7c95228e985039b808f9e5ed52e242fb0
sam atman <atmanistan@gmail.com>
2014-12-08 13:44:02 -0800

    range progresses

--- range.lua
+++ range.lua
@@ -1,11 +1,19 @@
 -- UTF-8 aware range generator
 
 -- The problem: Ranges as defined in Lpeg are between single bytes. This falls down badly in Unicode.
+--
 -- The solution: provide two strings. Construct a bytewise comparator that matches the range of the 
---     first bytes, followed by the range of the second, and so on. 
--- For ordinary Latin, this gives expected behavior. ["ab"--"cz"] would match any two bytes, provided the
+-- first bytes, followed by the range of the second, and so on. 
+--
+-- For ordinary Latin, this gives expected behavior. ["ab"-"cz"] would match any two bytes, provided the
 -- first is in a-c and the latter is in b-z. For UTF-8, this should in fact decompose into exactly 
 -- what we want. Back-adaptation to old regexes was keenly on the mind of the designers. 
+-- 
+-- Currently, we barf on a range like `["ab"-"c"]`. This precludes matching against Unicode characters of 
+-- different widths. PEGs are very flexible, and there should be a way to resolve this, at least in some
+-- useful cases. This should come up quite seldom, as the various code classes don't cross width boundaries
+-- for just this reason.
+
 
 require "lpeg"
 
@@ -25,7 +33,23 @@ inquotes = P"\"" * C((R"\35\255" + R"\0\33")^0) * P"\""
 --local 
 range_g = P"[" * inquotes * P"-" * inquotes * P"]"
 
-
+function makerange(first, second)
+	local from = {}
+	local to   = {}
+	if (string.len(first) == string.len(second)) then
+		for i = 1, string.len(first) do
+			from[i] = string.byte(first, i)
+			to[i] = string.byte(second, i) 
+		end
+		-- make into digit strings
+		for i = 1, string.len(first) do
+			print("From: ", from[i], " To: ", to[i])
+		end
+	else
+		error("Ranges must be of equal byte width")
+		return {}
+	end
+end
 --tests
 teststr = "\"χἏ☒➤\"" --any old weird stuff, in quotes
 ascii = "\"readable\""
@@ -34,3 +58,4 @@ print(range_s)
 print(match(inquotes,teststr))
 print(match(inquotes,ascii))
 print(match(range_g,range_s))
+makerange(match(range_g,range_s))
