5aabdc7197ec289ae2466dab4413563b5eaa89b0
sam atman <atmanistan@gmail.com>
2014-12-08 14:53:29 -0800

    got fancy there. need to write PEGylator for that nonsense. small sharp tools.

--- range.lua
+++ range.lua
@@ -17,38 +17,15 @@
 
 require "lpeg"
 
---local
-match = lpeg.match -- match a pattern against a string
-P = lpeg.P -- match a string literally
-S = lpeg.S  -- match anything in a set
-R = lpeg.R  -- match anything in a range
-C = lpeg.C  -- captures a match
-Ct = lpeg.Ct -- a table with all captures from the pattern
-Cg = lpeg.Cg -- a group capture
-V = lpeg.V -- create a variable within a grammar
-
---local
--- note: syntax should be `ab-cz`, not ["ab"-"cz"]. 
--- \" and \\ need to be handled in the usual way. 
-inquotes = P"\"" * C((R"\35\255" + R"\0\33")^0) * P"\""
-
-
---local 
-range_g = P"[" * inquotes * P"-" * inquotes * P"]"
-
---local
-
 local function makerange(first, second)
 	local patts = {}
 	local patt  = {}
 	if (string.len(first) == string.len(second)) then
 		for i = 1, string.len(first) do
-			patts[i] = R(string.sub(first,i,i)..string.sub(second,i,i))
-			print ("From: "..string.sub(first,i,i).." To: "..string.sub(second,i,i))
+			patts[i] = lpeg.R(string.sub(first,i,i)..string.sub(second,i,i))
 		end
 		patt = patts[1]
 		for i = 2, string.len(first) do
-		--	print "multi-byte"
 			patt = patt + patts[i]
 		end
 		return patt
@@ -58,19 +35,10 @@ local function makerange(first, second)
 	end
 end
 
+Ru = makerange
 --tests
 teststr = "\"χἏ☒➤\"" --any old weird stuff, in quotes
 ascii = "\"readable\""
-range_s = "[\"ab\"-\"cz\"]"
+greek = Ru("Α","Ω")
+print ("Greek ",lpeg.match(greek,"Β"))
 
-greek_s = "[\"Α\"-\"Ω\"]"
-beta = "Β"
-greek = makerange(match(range_g,greek_s))
-print ("Greek ",match(greek,beta))
-print(range_s) 
-print(match(inquotes,teststr))
-print(match(inquotes,ascii))
-print(match(range_g,range_s))
-makerange(match(range_g,range_s))
-in_range = "aq"
-print(match(makerange(match(range_g,range_s)),in_range))
