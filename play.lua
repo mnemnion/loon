0305e999ebd49b6179394c9470ce4ea99ecc27f9
sam atman <atmanistan@gmail.com>
2014-12-08 01:04:43 -0800

    much parse. so suppress. wow.

--- play.lua
+++ play.lua
@@ -1,14 +1,12 @@
+dofile "./tools/util.lua"
 require "lpeg"
 epnf = require "lua-luaepnf/src/epnf"
+
 
 function rl()
 	dofile "play.lua"
 end
 
--- Some tests
-
-list_string = "(foo 23 bar)"
-map  = "{ foo bar, baz 42}"
 
 match = lpeg.match -- match a pattern against a string
 P = lpeg.P -- match a string literally
@@ -18,33 +16,19 @@ C = lpeg.C  -- captures a match
 Ct = lpeg.Ct -- a table with all captures from the pattern
 V = lpeg.V -- create a variable within a grammar
 
-symbol = R'az'^1-- incorrect start
-number = R'09'^1 -- likewise
-WS     = P' '^0 + P'\n'^0 + P',' + P'\07'
-
-atom = WS * symbol * WS + WS * number * WS
-
 
 clua = epnf.define ( function (_ENV)
-	local symbol = R'az'^1-- incorrect start
-	local number = R'09'^1 -- likewise
-	local WS     = P' '^0 + P'\n'^0 + P',' + P'\07'
-	local atom   = WS * symbol * WS + WS * number * WS
 	START "form"
-	list    =   P'(' * V"element"^0 * P')' * WS  
-	element = atom + V"form" 
+	suppress ("element")
+    local WS_     = P' '^0 + P'\n'^0 + P',' + P'\07'
+    symbol = C(R'az'^1)-- incorrect start
+    number = C(R'09'^1) -- likewise 
+	atom   = WS_ * V"symbol" * WS_ + WS_ * V"number" * WS_
+	list    =   P'(' * V"element"^0 * P')' * WS_ 
+	element = V"atom" + V"form" 
 	form    = V"list"
 end)
---]]
 
 
-form = P{
-  "form" ;
-  list =   P'(' * V"element"^0 * P')' * WS  ;
-  element = atom + V"form" ;
-  form = V"list" ;
-}
-
-print(match(form,list_string))
-print(match(form,"()"))
-print(match(form,"(foo bar(baz bux))"))
+ ast = epnf.parsestring(clua, "(foo bar)")
+epnf.dumpast(ast)
