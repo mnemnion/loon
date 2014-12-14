33a447bd32ba567bfd16cada56073a0ae6220998
sam atman <atmanistan@gmail.com>
2014-12-13 19:20:33 -0800

    well that's a fine mess. cat_rule not needed.

--- src/pegylator/pegylator.lua
+++ src/pegylator/pegylator.lua
@@ -20,27 +20,31 @@ V = lpeg.V -- create a variable within a grammar
 	valid_sym = R"AZ" + R"az" + P"-" 
 	digit = R"09"
 	sym = valid_sym + digit
-	WS = P' '^0 + P'\n'^0 + P',' + P'\09'
+	WS = P' ' + P'\n' + P',' + P'\09'
 	symbol = (valid_sym^1 * sym^0)
 
 peg = epnf.define(function(_ENV)
 	START "rules"
-	SUPPRESS ("WS")
+	SUPPRESS ("WS", "rhs", "element")
 	--local Ru = Ru 
 	--valid_sym = C(R"AZ"^1) + C(R"az"^1) + C(P"-"^1) 
 	--digit = C(R"09"^1)
 	--sym = V"valid_sym" + digit
 	--symbol = V"valid_sym"^-1 * sym^0
-	local symbol = WS * C(symbol) * WS
-	rules = V"rule"
-	rule  = V"lhs" * P':' * V"rhs"
-	lhs   = symbol
-	rhs   = V"paren_rule" + V"atom_rule"  
-	paren_rule = symbol
+	local cat_space = WS^1
+	local WS = WS^0
+	local symbol =  C(symbol)
+	rules   = V"rule"
+	rule    = V"lhs" * P':' * V"rhs"
+	lhs     = WS * symbol * WS
+	rhs     =  (V"element" + V"cat_rule" + WS * V"atom_rule" * WS)^1
+	element =  WS * V"paren_rule" * WS  
+    paren_rule = WS * P"(" * WS * ( V"element"+ V"cat_rule")^1 * WS * P")"
+    cat_rule  = (V"element" + WS * V"atom_rule" ) * cat_space * V"element"^1
 	atom_rule = symbol
 end)
 
-grammar_s = [[ A : B / C
+grammar_s = [[ A : B C ( D E ) /
 			  C : "D" 
 			  D : E F G
 ]]
@@ -48,7 +52,7 @@ grammar_s = [[ A : B / C
 rule_s  = [[A : B]]
 
 dump_ast (match(peg,grammar_s))
-dump_ast (match(peg,grammar_s))
+--print (match(peg,grammar_s))
 symbol_s = "rgsr09gaoijfsdfkrtjhaADSFASDFAr--"
 
 --print (match(symbol, symbol_s))
