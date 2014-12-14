35b91093cd76ce6224f8b0bc7b4fed1bcbe4cb99
sam atman <atmanistan@gmail.com>
2014-12-13 20:43:28 -0800

    improved?

--- src/pegylator/pegylator.lua
+++ src/pegylator/pegylator.lua
@@ -25,22 +25,26 @@ V = lpeg.V -- create a variable within a grammar
 
 peg = epnf.define(function(_ENV)
 	START "rules"
-	SUPPRESS ("WS", "rhs", "element")
+	SUPPRESS ("WS", "rhs", "cat_space" )
 	--local Ru = Ru 
 	--valid_sym = C(R"AZ"^1) + C(R"az"^1) + C(P"-"^1) 
 	--digit = C(R"09"^1)
 	--sym = V"valid_sym" + digit
 	--symbol = V"valid_sym"^-1 * sym^0
+	local cat_space = WS^1
 	local WS = WS^0
 	local symbol =  C(symbol)
 	rules   = V"rule"
 	rule    = V"lhs" * P':' * V"rhs"
 	lhs     = WS * symbol * WS
-	rhs     = V"element"^1
-	element  = V"choice" * WS * V"choice"^0
-	choice = V"factor" * (P"/" * V"factor") ^0
-	factor = V"atom_rule" * (P"(" * V"element" * P")")^0 * V"element"^0
-	atom_rule = WS * symbol * WS
+	rhs     = V"element" * V"more_elements"
+	more_elements  = V"choice"  
+			       + V"cat"
+		       	   + V"factor"
+	choice = P"/" * V"element" * V"more_elements"^0
+	cat = cat_space *  V"element" * V"more_elements"^0
+	factor = V"element" + (P"(" * V"element" * P")") 
+	element = WS *symbol
 end)
 
 grammar_s = [[ A : B C ( D E ) / F G H
@@ -48,10 +52,14 @@ grammar_s = [[ A : B C ( D E ) / F G H
 			  D : E F G
 ]]
 
-rule_s  = [[A : B]]
+rule_s  = [[A:B C(D E)/(F G H)
+			  C : "D" 
+			  D : E F G
+]]
 
 dump_ast (match(peg,grammar_s))
 dump_ast (match(peg,rule_s))
+dump_ast (match(peg,"A     :B C D E "))
 symbol_s = "rgsr09gaoijfsdfkrtjhaADSFASDFAr--"
 
 --print (match(symbol, symbol_s))
