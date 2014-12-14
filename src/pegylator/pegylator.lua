436442abc124a2402ba6fc9db5ecf9b9a2badca2
sam atman <atmanistan@gmail.com>
2014-12-13 21:05:04 -0800

    sets precendence correctly

--- src/pegylator/pegylator.lua
+++ src/pegylator/pegylator.lua
@@ -25,26 +25,27 @@ V = lpeg.V -- create a variable within a grammar
 
 peg = epnf.define(function(_ENV)
 	START "rules"
-	SUPPRESS ("WS", "rhs", "cat_space" )
+	SUPPRESS ("WS", "rhs", "cat_space" , "element" ,"more_elements", "pattern")
 	--local Ru = Ru 
 	--valid_sym = C(R"AZ"^1) + C(R"az"^1) + C(P"-"^1) 
 	--digit = C(R"09"^1)
 	--sym = V"valid_sym" + digit
 	--symbol = V"valid_sym"^-1 * sym^0
 	local cat_space = WS^1
-	local WS = WS^0
+	local WS = C(WS^0)
 	local symbol =  C(symbol)
 	rules   = V"rule"
-	rule    = V"lhs" * P':' * V"rhs"
-	lhs     = WS * symbol * WS
-	rhs     = V"factor" * V"more_elements"
-	more_elements  = V"choice"  
-			       + V"cat"
-		       	   + V"factor"
-	choice = P"/" * V"element" * V"more_elements"^0
+	rule    = V"lhs"  * V"rhs"
+	lhs     = WS * symbol * WS * P":"
+	rhs     = V"element" * V"more_elements"^0 
+	more_elements  = (V"choice"  
+			       + V"cat")
+		       	   --+ V"factor"
+	choice = WS * P"/" * V"element" * V"more_elements"^0
 	cat = cat_space *  V"element" * V"more_elements"^0
-	factor = V"element" + (P"(" * V"element" * P")") 
-	element = WS *symbol
+	element = V"pattern" + V"factor"
+	factor  = ( WS * P"(" * WS * V"rhs" * WS * P")") 
+	pattern = WS * symbol - V"lhs"
 end)
 
 grammar_s = [[ A : B C ( D E ) / F G H
@@ -58,8 +59,8 @@ rule_s  = [[A:B C(D E)/(F G H)
 ]]
 
 dump_ast (match(peg,grammar_s))
-dump_ast (match(peg,rule_s))
-dump_ast (match(peg,"A     :B C D E "))
+--dump_ast (match(peg,rule_s))
+--dump_ast (match(peg,"A     :B C D E "))
 symbol_s = "rgsr09gaoijfsdfkrtjhaADSFASDFAr--"
 
 --print (match(symbol, symbol_s))
