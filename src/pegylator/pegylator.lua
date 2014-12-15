b6494f5eda2328c1c7dc7c2088780a375b660a36
sam atman <atmanistan@gmail.com>
2014-12-15 11:57:47 -0800

    empty string

--- src/pegylator/pegylator.lua
+++ src/pegylator/pegylator.lua
@@ -81,9 +81,9 @@ peg = epnf.define(function(_ENV)
 	enclosed =  V"literal"
 		     +  V"set"
 		     +  V"range"
-    literal =  P'"' * (string + P"") * P'"'   -- make into real string
-    set     =  P"{" * set_c^1 * P"}"    -- should match all char and escaped "}"
-    range   =  P"[" * range_c * P"]"   -- make into real range
+    literal =  P'"' * (string + P"") * P'"'  
+    set     =  P"{" * set_c^1 * P"}"   
+    range   =  P"[" * range_c * P"]"   
 	optional      =  symbol * P"*"
 	more_than_one =  symbol * P"+"
 	maybe         =  symbol * P"?"
