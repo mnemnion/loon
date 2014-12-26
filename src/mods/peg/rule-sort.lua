8519e5f5f7572e48fc30df59135b46d7da45da40
sam atman <atmanistan@gmail.com>
2014-12-26 14:30:52 -0800

    again

--- src/mods/peg/rule-sort.lua
+++ src/mods/peg/rule-sort.lua
@@ -9,55 +9,45 @@ local clear = tostring(ansi.clear)
 local sort = {}
 
 local function rule_tables(node)
--- returns two Forests with the LHS and RHS symbols indexed
 	local lhs = node:select"lhs"
-	for i,v in ipairs(lhs) do
-		lhs[v.val] = i -- lookup Node, get index.
-	end
 	local rhs = node:select"rhs":pick"atom" 
-	return lhs, rhs
+	local ndx = {}
+	for i,v in ipairs(lhs) do
+		ndx[i] = {}
+		ndx[i].lhs = lhs[i]
+		if v.val then
+			ndx[i].val = v.val  -- lookup Node, get index.
+		elseif v[1].id == "hidden_pattern" then
+				print ("Hidden Pattern",v[1].val)
+				ndx[i].val = v[1].val
+		end
+		local rights = {}
+		for j,v in ipairs(rhs[i]) do
+			rights[j] = v.val
+		end
+		ndx[i].rhs = rights
+	end
+	
+	return ndx
 end
 
 
-local function detect_recursion(index, lhs, rhs)
-	local match = lhs[index]
-	local visit, visited = {},{}
-	local function find_match(match,visit,lhs, rhs)
-		print ("Matching ", white, match.val, clear)
-		print ("Visitor: ", red, lhs[lhs[visit]].val, clear)
-		for i,v in ipairs(rhs[lhs[visit]]) do
-			print ("     visiting: ", red, v.val, clear)
-			if match.val == v.val or lhs[lhs[v.val]].isrecursive then
-				print (red, "MATCHED", clear)
-				match.isrecursive = true
-			end
-		end
-	end
-	-- lookup match on rhs
-	--print(rhs[index])
-	for i,v in ipairs(rhs[index]) do
-		if v.val == match.val then 
-			print ("match detected: ", v.id, " matches", match.val)
-			match.isrecursive = true
-			return
-		else 
-			visit[i] = v.val
-		end
-	end
-	for i,v in ipairs(visit) do
-		local matched = find_match(match, v, lhs, rhs)
-		print ("Matched: ", matched)
-	end
+local function detect_recursion(ndx, i)
+		
 end
 
 
 function sort.sort (node)
 -- Divides rules into Regular and Recursive, decorating accordingly on LHS.
-	local lhs, rhs = rule_tables(node)
-	for i,v in ipairs(lhs) do 
-		detect_recursion(i,lhs,rhs)
-		print("LHS:", lhs[i])
-		print("RHS:", rhs[i],"\n")
+	local ndx = rule_tables(node)
+	for i,v in ipairs(ndx) do 
+		detect_recursion(ndx,i)
+		print("LHS: ", ndx[i].val)
+		io.write("RHS: ")
+		for j,v in ipairs(ndx[i].rhs) do
+			io.write(ndx[i].rhs[j]," ")
+		end
+		io.write "\n"
 	end
 end
 
