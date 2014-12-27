d4f250006915a8f8c06ad5b037a6b8c41a7f3dec
sam atman <atmanistan@gmail.com>
2014-12-26 20:25:31 -0800

    I *think* that's working. My peg grammar is, er, no longer current...

--- src/mods/peg/rule-sort.lua
+++ src/mods/peg/rule-sort.lua
@@ -22,6 +22,7 @@ local function rule_tables(node)
 		elseif v[1].id == "hidden_pattern" then
 				ndx[i].val = v[1].val
 		end
+		ndx[ndx[i].val] = lhs[i] 
 		local rights = Set{}
 		for j,v in ipairs(rhs[i]) do
 			rights  = rights + Set{v.val}
@@ -53,17 +54,39 @@ end
 
 function sort.sort (node)
 -- Divides rules into Regular and Recursive, decorating accordingly on LHS.
+
 	local ndx = rule_tables(node)
 	for i,v in ipairs(ndx) do 
 		detect_recursion(ndx,i)
 	end
+	local cursors = Set{}
 	for i,v in ipairs(ndx) do
 		print(ndx[i].val, "=", ndx[i].rhs)
 		if ndx[i].rhs[ndx[i].val] then
 			print "is RECURSIVE!"
 			ndx[i].lhs.isrecursive = true
+			cursors = cursors + Set{ndx[i].val}
 		end
 	end
+	print ("CURSORS: ", cursors)
+	local old_cursors = 0
+	while (old_cursors < Set.len(cursors)) do
+		print "IN WHILE"
+		old_cursors = Set.len(cursors)
+		for i,v in ipairs(ndx) do
+			for val, _ in pairs(ndx[i].rhs) do
+				print("for ",val)
+				if cursors[val] then
+					if not (cursors[ndx[i].val]) then
+						print(ndx[i].val, "IS ALSO RECURSIVE!")
+						ndx[i].lhs.isrecursive = true
+						cursors = cursors + Set{ndx[i].val}
+					end
+				end
+			end
+		end
+	end
+	print("final cursors: ", cursors)
 end
 
 return sort
