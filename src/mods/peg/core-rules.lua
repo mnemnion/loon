fdc99eef3ee88716a308cf6501cad5070b9d80cc
sam atman <atmanistan@gmail.com>
2015-01-13 19:33:17 -0800

    string escaping :-"

--- src/mods/peg/core-rules.lua
+++ src/mods/peg/core-rules.lua
@@ -2,10 +2,10 @@
 
 -- A collection of useful regular patterns
 
-local lpeg = require "lpeg"
+ lpeg = require "lpeg"
 local epeg = require "peg/epeg"
 
-local match = lpeg.match -- match a pattern against a string
+ match = lpeg.match -- match a pattern against a string
 local P = lpeg.P -- match a string literally
 local S = lpeg.S  -- match anything in a set
 local R = epeg.R  -- match anything in a range
@@ -14,6 +14,7 @@ local C = lpeg.C  -- captures a match
 local Csp = epeg.Csp -- captures start and end position of match
 local Ct = lpeg.Ct -- a table with all captures from the pattern
 local V = lpeg.V -- create a variable within a grammar
+local Cmt = lpeg.Cmt
 
 local core = {}
 
@@ -24,3 +25,25 @@ local int   = digit^1
 local float = digit^1 
 			* P"." * digit^1 
 		    * ((P"e" + P"E") * digit^1)^0
+
+ escape =  -P"\\" * P(1) + P"\\" * P(1)
+
+local string_single = P"'" * (-P"'" * escape)^0 * P"'"
+local string_double = P'"' * (-P'"' * escape)^0 * P'"'
+local string_back   = P"`" * (-P"`" * escape)^0 * P"`"
+
+local strings = string_single + string_double + string_back
+
+local strtest = require "peg/strings"
+
+function assertions(strs)
+	for k, v in pairs(strs) do 
+		print ("testing", k)
+		assert(#v+1 == (match(strings,v)))
+		print ("\n",v, " is good")
+	end
+end
+
+assertions(strtest)
+
+
--- src/mods/peg/strings.lua
+++ src/mods/peg/strings.lua
@@ -1,14 +1,14 @@
 
 local strings = {}
 
-strings.simple_d = '"a simple double string"'
+strings.simple_d = '"a string"'
 strings.simple_s = "'a simple single string'"
 strings.simple_b = "`a simple backtick string`"
-strings.esc_d = '"an \" esc\'d double string "'
-strings.esc_s = "' an \" esc\'d single string'"
-strings.esc_h = "` an \` esc\'d backtick \" string"
-string.end_d = '"challenge double string\\"'
-string.end_s = "'challenge single string\\'"
-string.end_h = "'challenge backtick string\\`"
+strings.esc_d = '"an \\" esc\\\'d double string "'
+strings.esc_s = "' an \\\" esc\\\'d single string'"
+strings.esc_h = "` an \\ esc\\\'d backtick \\\" string`"
+string.end_d = '"challenge double string\\\\"'
+string.end_s = "'challenge single string\\\\'"
+string.end_h = "`challenge backtick string\\\\`"
 
 return strings
