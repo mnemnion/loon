26c60237700afb63f1561119c5ac6568e1e291b8
sam atman <atmanistan@gmail.com>
2015-01-14 17:38:07 -0800

    failstrings

---
+++ src/mods/peg/failstrings.lua
@@ -0,0 +1,17 @@
+
+local strings = {}
+
+strings.simple_d = '"a string"~~'
+strings.simple_s = "'a simple single string'~~"
+strings.simple_b = "`a simple backtick string`~~"
+strings.esc_d = '~~"an \\" esc\\\'d double string "'
+strings.esc_s = "~~' an \\\" esc\\\'d single string'"
+strings.esc_h = "~~` an \\ esc\\\'d backtick \\\" string`"
+strings.end_d = [[~"challenge double string\\\\"~]]
+strings.end_s = "~'challenge single string\\\\'~"
+strings.end_h = "~`challenge backtick string\\\\`~"
+strings.trivial_s = "'"
+strings.trivial_d = '"'
+strings.trivial_h = "`"
+
+return strings
---
+++ src/mods/test.lua
@@ -0,0 +1,47 @@
+local epeg = require "peg/epeg"
+local match = epeg.match
+local strings = require "peg/strings"
+local failstrings = require "peg/failstrings"
+local core = require "peg/core-rules"
+local say = require "say"
+local ansi = require "ansi"
+
+local function whole_match(state, args)
+	local matched, hmm = match(args[2],args[1])
+	if matched == nil then 
+		matched = 0 end -- this is derpy
+	if (#args[1]+1 == matched) then
+		return true
+	elseif (#args[1]+1 > matched) then
+		args[2] = ansi.magenta..args[1]:sub(1,matched-1)..ansi.clear
+		return false
+	else
+		args[2] = "something is badly broken"
+	end
+end
+
+say:set("assertion.whole_match.positive", "Expected match on whole string:\n%s Got:\n%s")
+say:set("assertion.whole_match.negative", "Expected failure of whole match")
+assert:register("assertion","whole_match",whole_match,"assertion.whole_match.positive","assertion.whole_match.negative")
+
+describe("tests over PEGylator", function()
+	it("String succeeds",function ()
+		for _,v in pairs(strings) do
+			assert.whole_match(v,core.strings)
+		end
+	end)
+	it("String failures", function()
+		for _,v in pairs(failstrings) do
+			assert.is_not.whole_match(v,core.strings)
+		end
+	end)
+	it("int asserts", function()
+		assert.equal(#("123")+1, (match(core.int,"123")))
+	end)
+	it("Float asserts", function()
+		pending "Add float tests"
+	end)
+	it("Letter asserts", function()
+		pending "Add letter tests"
+	end)
+end)
